49e686ff246b1de3239230425d1f186f6a532486
tanpm <tanpm@mobiletech.vn>
2024-01-10 08:40:57 +0700

    feat: support for banner in-app

--- pushdy/src/main/java/com/pushdy/core/entities/PDYPlayer.kt
+++ pushdy/src/main/java/com/pushdy/core/entities/PDYPlayer.kt
@@ -132,4 +132,35 @@ open class PDYPlayer(val ctx: Context, clientKey : String, deviceID:String?) : P
             throw this.noContextWasSetException()
         }
     }
+
+    open fun subscribe(applicationId: String,playerID:String, completion:((response:JsonElement?) -> Unit?)?, failure:((code:Int, message:String?) -> Unit?)?) : PDYRequestSingleton {
+        if (this.context != null) {
+            val request = PDYRequestSingleton.getInstance(context!!)
+            request.post("${this.baseUrl()}/application/${applicationId}/player/${playerID}/subscribe", this.defaultHeaders(), null, { response:JsonElement? ->
+                completion?.invoke(response)
+            }, { code:Int, message:String? ->
+                failure?.invoke(code, message)
+            })
+            return request
+        }
+        else {
+            throw this.noContextWasSetException()
+        }
+    }
+
+    fun trackBanner(applicationId: String, playerID: String, data: JsonElement?, completion:((response:JsonElement?) -> Unit?)?, failure:((code:Int, message:String?) -> Unit?)?): PDYRequestSingleton {
+        if (this.context != null) {
+            val request = PDYRequestSingleton.getInstance(context!!)
+            request.post("${this.baseUrl()}/application/${applicationId}/player/${playerID}/subscribe", this.defaultHeaders(), data, { response:JsonElement? ->
+                completion?.invoke(response)
+            }, { code:Int, message:String? ->
+                failure?.invoke(code, message)
+            })
+
+            return request
+        }
+        else {
+            throw this.noContextWasSetException()
+        }
+    }
 }
